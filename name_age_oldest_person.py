c65ad82dc3f74cad1732ea1f804354a827648710
clarencejeddmaas <clarencemaas74@gmail.com>
2024-10-22 19:26:48 +0800

    loop for user's name and age, storing name and age in list

--- name_age_oldest_person.py
+++ name_age_oldest_person.py
@@ -8,3 +8,25 @@ def valid_age(age):
 
 # 3. Initialize list for storing data
 people_info = []
+
+# 4. Loop for user input
+while True:
+    name = input("Enter your name: ").strip()
+    
+    if not valid_name(name):
+        print("Error! Only alphabetic characters allowed.")
+        continue
+
+    # 5. Loop for age input
+    while True:
+        age = input("Please enter your age: ").strip()
+        
+        if not valid_age(age):
+            print("Error! Only non-negative numbers are allowed.")
+            continue 
+        else:
+            age = int(age) 
+            break
+    
+    # 6. Store name and age in list
+    people_info.append({"name": name, "age": age})
